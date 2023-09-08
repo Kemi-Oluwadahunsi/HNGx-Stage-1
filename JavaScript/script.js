edf4cc8302935649844ce3208a58d822d4bac53f
Oluwakemi Oluwadahunsi <kemilat50@gmail.com>
2023-09-08 17:01:06 +0800

    Updated all codes

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -12,27 +12,6 @@ const dayName = dayNames[dayOfWeek];
 const dayOfWeekElement = document.getElementById("dayOfTheWeek");
 dayOfWeekElement.textContent = `Current Day : ${dayName}`;
 
-
-// Get the current time
-
-// function updateClock() {
-//     const now = new Date();
-//     const hours = String(now.getHours()).padStart(2, '0');
-//     const minutes = String(now.getMinutes()).padStart(2, '0');
-//     const seconds = String(now.getSeconds()).padStart(2, '0');
-
-//     document.getElementById('hours').textContent = hours; // Get hour
-//     document.getElementById('minutes').textContent = minutes; // Get minutes
-//     document.getElementById('seconds').textContent = seconds; // Get seconds
-// }
-
-// // Update the clock every second
-// setInterval(updateClock, 1000);
-
-// // Initial update
-// updateClock();
-
-
 function updateTime() {
     const now = new Date();
     
